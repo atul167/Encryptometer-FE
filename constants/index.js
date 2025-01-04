c4272b9845dfdc2375a2a62874ccfb908ea3413e
Divakar Mahanthi <mahanthidivakarnaidu@aesl.in>
2025-01-04 19:41:09 +0530

    Updated aes

--- constants/index.js
+++ constants/index.js
@@ -1,18 +1,13 @@
 export const PORT = 1000;
 export const BACKEND_URL = "http://localhost";
 export const CRYPTO_ALGOS = [
-    { value: "aes", label: "AES" },
-    { value: "ssh", label: "SSH" },
-    { value: "rsa", label: "RSA" },
-    { value: "chacha", label: "ChaCha" },
+  { value: "aes", label: "AES" },
+  { value: "ssh", label: "SSH" },
+  { value: "rsa", label: "RSA" },
+  { value: "chacha", label: "ChaCha" },
 ];
-export const DECRYPT_KEY_AES = "239299bd1ecac5126e6387cb335ef575b88613f13bb75ba46031d9f3b86f6842"
-// export const DECRYPT_KEY_RSA = "RSASecretKey456";    
-// export const DECRYPT_KEY_SSH = "SSHSecretKey789";
-// export const SAMPLE_TEXT = "U2FsdGVkX18scpMIUOWxUr16+ldWGszS5R0BZH6CnQ7RQt1qUcHO2m1Zv3nodT2ueVApNVbnLwkJJz5ENyvsFg=="; 
 
 export const OBJ = {
-    greeting: "Hello",
-    recipient: "World",
+  greeting: "Hello",
+  recipient: "World",
 };
-
